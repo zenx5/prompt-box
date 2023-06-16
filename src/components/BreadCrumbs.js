2afc041a73805559fdf3ecce3dae072eb66c6a24
Octavio Martinez <omartinez1618@gmail.com>
2023-06-16 14:15:12 -0400

    feat(BreadCrumbs.js): add BreadCrumbs component to display the current path and allow navigation to previous paths.

---
+++ src/components/BreadCrumbs.js
@@ -0,0 +1,24 @@
+import { Box, ButtonBase, Typography } from "@mui/material";
+import { useStorage } from "../services/StorageService";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+
+export default function BreadCrumbs() {
+    const { path, back, getBy } = useStorage()
+
+    const joiner = (items, separator) => {
+        return Array.from( Array(2*items.length - 1), (el, index) => index%2?separator:items[index/2] )
+    }
+
+    const handlerBack = (index) => {
+        if( path.length>1 && index!=path.length-1 ){
+            back( path.length - (index + 1) )
+        }
+    }
+
+    return <Box sx={{ display:'flex', mx:5, p:0, alignItems:'center' }}>
+        {joiner( path.map( (id, index) => {
+            const name = getBy('name', 'id', id)
+            return <ButtonBase key={id} sx={{ fontSize:'1.3rem' }} onClick={()=>handlerBack(index)}>{name ?? 'root'}</ButtonBase>
+        } ), <ChevronRight />)}
+    </Box>
+}
